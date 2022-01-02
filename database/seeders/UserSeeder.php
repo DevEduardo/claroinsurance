c864c57795f6fe6315e5a482dd13307943f97b85
Eduardo Saes <eduardoargenis383@gmail.com>
2022-01-02 10:53:43 -0400

    create database

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrador',
+            'email' => env('USER_ADMIN_EMAIL'),
+            'email_verified_at' => now(),
+            'password' => bcrypt(env('USER_ADMIN_PASSWORD')),
+        ]);
+    }
+}
